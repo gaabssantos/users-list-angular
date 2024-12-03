7bd822d714855cbc2db3e44c5853130304f13bff
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-12-03 20:48:22 -0300

    refactor: Added loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { UserListServiceService } from './services/user-list-service.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 interface IUserProps {
   firstName: string;
@@ -17,16 +18,19 @@ interface IUser {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, UserComponent, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   public usersMock: any;
+  public loading = signal(false);
 
   constructor(private service: UserListServiceService) {
+    this.loading.set(true);
     this.service.getUsers().subscribe((data: IUser) => {
       this.usersMock = data;
+      this.loading.set(false);
     });
   }
 
